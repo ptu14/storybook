2ac66891b90a826c899ac6e84fd7138ce495700f
Norbert de Langen <ndelangen@me.com>
2023-06-16 16:50:07 +0200

    made node-logger CJS only, prebundle everything, treeshake CJS output as well

--- scripts/prepare/bundle.ts
+++ scripts/prepare/bundle.ts
@@ -116,6 +116,8 @@ const run = async ({ cwd, flags }: { cwd: string; flags: string[] }) => {
     tasks.push(
       build({
         silent: true,
+        treeshake: true,
+        splitting: true,
         entry: allEntries,
         watch,
         outDir,
